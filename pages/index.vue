9c1aa0a1dc17322c433eab81b85050389c60203f
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2024-02-01 10:03:51 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <nuxt-link to="/intro">
+            <button>Commencer</button>
+        </nuxt-link>
+       
+    </div>
+</template>
+
+<style scoped>
+
+</style>
